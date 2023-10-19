08d133f362e93e536b0ad153c32b7b1d2341f9cb
dellis <david.ellis@iongroup.com>
2023-10-19 15:34:08 +0100

    david: azure

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup
 from pathlib import Path
 import os,sys
-version = "0.0.1"
-
 setup (
     name="bling",
-    version=version,
-    maintainer=u'davidmarkellis@yahoo.com',
+#    maintainer=u'davidmarkellis@yahoo.com',
 
-    entry_points={
-        'console_scripts': [
-            'bling=bling.cmdline:main'
-        ]
-    }
+#    entry_points={
+#        'console_scripts': [
+#            'bling=bling.cmdline:main'
+#        ]
+#    }
 )
