d36dc407af0cbe97744ec8699d912e84f452de19
davidmellis <david.ellis@iongroup.com>
2023-10-20 16:47:56 +0100

    david: setting up git push script

--- push.sh
+++ push.sh
@@ -6,5 +6,4 @@ git config --global user.password "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"
 git config --list --show-origin 
 #            pip install bumpversion && 
 #            bumpversion --allow-dirty --commit --tag --config-file=$(Build.Repository.LocalPath)/.bumpversion.toml  --list patch &&
-            git push origin HEAD:main
- 
+            git push origin HEAD:main 
