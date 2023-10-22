f4d87d0e2be6473a9a72e248571dc87c89f8f225
davidmellis <david.ellis@iongroup.com>
2023-10-22 11:49:21 +0100

    david: tidyup push.sh

--- push.sh
+++ push.sh
@@ -2,8 +2,7 @@
 echo "Hello World"
 git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "davidmellis" &&
-git config --global user.password "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"
 git config --list --show-origin 
-#            pip install bumpversion && 
-#            bumpversion --allow-dirty --commit --tag --config-file=$(Build.Repository.LocalPath)/.bumpversion.toml  --list patch &&
-            git push origin HEAD:main 
+pip install bumpversion && 
+bumpversion --allow-dirty --commit --tag --config-file=$(Build.Repository.LocalPath)/.bumpversion.toml  --list patch &&
+git push origin HEAD:main 
