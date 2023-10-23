f2aa099e979f33ec5abd2a5ff360d22aef73214d
davidmellis <david.ellis@iongroup.com>
2023-10-23 10:28:56 +0100

    david: more more fixes - one hopes

--- push.sh
+++ push.sh
@@ -2,12 +2,14 @@
 echo "Hello World"
 git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "davidmellis"
-git config --global remote.origin.url=git@github.com:davidmellis/testci.git
-echo "Hello: $arg1"
 
+git remote add origin https://github.com/davidmellis/testci.git
+
+# git config --global remote.origin.url=git@github.com:davidmellis/testci.git
+echo "Hello: $arg1"
 git config --list --show-origin 
 
 
 pip install bumpversion
-bumpversion --allow-dirty --commit --tag --config-file=$arg1/.bumpversion.toml --list patch
+bumpversion --allow-dirty --commit --tag --config-file="$arg1/.bumpversion.toml" --list patch
 git push origin HEAD:main
