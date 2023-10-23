bdbe97198dea86fd066c3144f613d4482216288b
davidmellis <david.ellis@iongroup.com>
2023-10-23 10:23:37 +0100

    david: more more debug

--- push.sh
+++ push.sh
@@ -3,9 +3,11 @@ echo "Hello World"
 git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "davidmellis"
 git config --global remote.origin.url=git@github.com:davidmellis/testci.git
+echo "Hello: $arg1"
 
-echo "config: $arg1/.bumpversion.toml"
 git config --list --show-origin 
+
+
 pip install bumpversion
 bumpversion --allow-dirty --commit --tag --config-file=$arg1/.bumpversion.toml --list patch
 git push origin HEAD:main
