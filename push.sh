c9422aea553714a31266024487690f270378adfb
davidmellis <david.ellis@iongroup.com>
2023-10-23 10:05:38 +0100

    david: debugging to pick up bump file

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "davidmellis"
 git config --global remote.origin.url=git@github.com:davidmellis/testci.git
 
+echo "config: $arg1/.bumpversion.toml"
 git config --list --show-origin 
 pip install bumpversion
 bumpversion --allow-dirty --commit --tag --config-file=$arg1/.bumpversion.toml --list patch
