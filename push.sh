9933898f47698fd062c74f8b1c27ed4166da75dd
davidmellis <david.ellis@iongroup.com>
2023-10-23 08:49:04 +0100

    david: take out &&

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 echo "Hello World"
 git config --global user.email "david.ellis@iongroup.com"
-&& git config --global user.name "davidmellis"
-&& git config --list --show-origin 
-&& pip install bumpversion
-&& bumpversion --allow-dirty --commit --tag --config-file=$arg1/.bumpversion.toml  --list patch &&
-git push origin HEAD:main 
+git config --global user.name "davidmellis"
+git config --list --show-origin 
+pip install bumpversion
+bumpversion --allow-dirty --commit --tag --config-file=$arg1/.bumpversion.toml --list patch
+git push origin HEAD:main
