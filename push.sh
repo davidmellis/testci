dbf9ff236c91e04514566c120bba6e878761b3ab
davidmellis <david.ellis@iongroup.com>
2023-10-23 11:11:10 +0100

    david: remove push to stop continous builds

--- push.sh
+++ push.sh
@@ -12,10 +12,9 @@ git remote set-url origin git@github.com:davidmellis/testci.git
 
 # https://github.com/davidmellis/testci.git
 
-# git config --global remote.origin.url=git@github.com:davidmellis/testci.git
 git config --list --show-origin 
 
 
 pip install bumpversion
 bumpversion --allow-dirty --commit --tag --config-file="$bumpfile" --list patch
-git push origin HEAD:main
+# git push origin HEAD:main
