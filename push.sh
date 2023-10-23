5c96427319c7b37363d6af51af40a15ba0f6f759
davidmellis <david.ellis@iongroup.com>
2023-10-23 09:52:32 +0100

    david: fixing url to be ssh

--- push.sh
+++ push.sh
@@ -2,7 +2,9 @@
 echo "Hello World"
 git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "davidmellis"
+git config --global remote.origin.url=git@github.com:davidmellis/testci.git
+
 git config --list --show-origin 
 pip install bumpversion
-bumpversion --allow-dirty --commit --tag --config-file=$arg0/.bumpversion.toml --list patch
+bumpversion --allow-dirty --commit --tag --config-file=$arg1/.bumpversion.toml --list patch
 git push origin HEAD:main
