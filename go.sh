f373489b1c59e83b2229d0d9af0da5b6e99bdf11
David Ellis <david.ellis@iongroup.com>
2023-11-14 10:21:16 +0000

    commitMessage

--- go.sh
+++ go.sh
@@ -8,11 +8,11 @@ git config --global user.name "David Ellis"
 git remote set-url origin git@github.com:davidmellis/testci.git 
 git config --global --add safe.directory '*'                    
 git remote -v 
-changeversion --bump micro 
+~/.local/bin/changeversion --bump micro 
 # git checkout ref/heads/main 
 git add .
 git add VERSION
-changeversion --tag                                             
+~/.local/bin/changeversion --tag                                             
 git status                                                      
 git pull --rebase origin main
 git push -u origin main
