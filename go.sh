8346db6905d66f2ff6e0e7888618ecc1d22214d4
David Ellis <david.ellis@iongroup.com>
2023-11-14 09:48:11 +0000

    david: try to fix git push again

--- go.sh
+++ go.sh
@@ -2,14 +2,13 @@ git branch -a
 pip config --user set global.index-url http://18.170.223.248:8080/
 pip install --trusted-host=18.170.223.248 changeversion
 pip list
-ls -la ~/.local/bin/
 git config --global user.email "david.ellis@iongroup.com"       
 git config --global user.name "David Ellis"                     
 git remote set-url origin git@github.com:davidmellis/testci.git 
 git config --global --add safe.directory '*'                    
 git remote -v 
 changeversion --bump micro 
-git checkout ref/heads/main 
+# git checkout ref/heads/main 
 git add .
 git add VERSION
 changeversion --tag                                             
