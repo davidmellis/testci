733f749902653fb487dfd563b8a170977f50349f
David Ellis <david.ellis@iongroup.com>
2023-11-14 08:52:03 +0000

    david: small changes to debug

--- go.sh
+++ go.sh
@@ -6,13 +6,13 @@ git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "David Ellis"                     
 git remote set-url origin git@github.com:davidmellis/testci.git 
 git config --global --add safe.directory '*'                    
-git remote -v                                                   
-~/.local/bin/changeversion --bump micro 
+git remote -v 
+changeversion --bump micro 
 git checkout ref/heads/main 
 git add .
 git add VERSION
-~/.local/bin/changeversion --tag                                             
+changeversion --tag                                             
 git status                                                      
 git pull --rebase origin main
-git push -u origin main                                         
+git push -u origin main
 
