90c195e512541383e1e92377dbb75227a289885a
David Ellis <david.ellis@iongroup.com>
2023-11-14 11:00:37 +0000

    david: more changes to test script

--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 git remote -v 
 git branch -a
 pip config --user set global.index-url http://18.170.223.248:8080/
-pip install --trusted-host=18.170.223.248 changeversion
+pip install changeversion
 pip list
 git config --global user.email "david.ellis@iongroup.com"       
 git config --global user.name "David Ellis"                     
