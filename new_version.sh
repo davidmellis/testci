7abe78e60380e8653f984928ab14963b38094b6c
davidmellis <david.ellis@iongroup.com>
2023-12-06 11:23:51 +0000

    david: tidyup of new_version logic

--- new_version.sh
+++ new_version.sh
@@ -8,15 +8,15 @@ pip install changeversion
 pip list
 
 git config --global user.email "david.ellis@iongroup.com"       
-git config --global user.name "David Ellis"                     
+git config --global user.name "davidmellis" 
 git remote set-url origin git@github.com:davidmellis/testci.git 
 git config --global --add safe.directory '*'                    
 git remote -v 
 
 changeversion --micro $BUILD_ID
+hangeversion --tag
 git add .
 git add VERSION
-changeversion --tag
 git status                  
 git pull --rebase origin main
 git push --tags  -u origin HEAD:main
