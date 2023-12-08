6c107e8fe4a7f70217e00cd7333cf9d61338ecfa
davidmellis <david.ellis@iongroup.com>
2023-12-08 09:43:51 +0000

    changeversion to v3.2.942262  ***NO_CI***

--- new_version.sh
+++ new_version.sh
@@ -1,6 +1,3 @@
-git remote -v 
-git branch -a
-
 pip config set global.index-url "http://18.170.223.248:8080/simple/"
 pip config set global.trusted-host "18.170.223.248"
 
@@ -11,16 +8,12 @@ git config --global user.email "david.ellis@iongroup.com"
 git config --global user.name "davidmellis"
 git remote set-url origin git@github.com:davidmellis/testci.git
 git config --global --add safe.directory '*' 
-git remote -v
 
 changeversion --micro $BUILD_ID
-changeversion --tag
+changeversion --tag --push
 git add .
 git add VERSION
 git status                  
-git pull --rebase origin main
-git push --tags  -u origin HEAD:main
-
-# git log -n3
+#git pull --rebase origin main
+#git push --tags  -u origin HEAD:main
 echo "PUSHING NOW COMPLETE"
-git show
